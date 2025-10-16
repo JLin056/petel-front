77e2020ed1bada32d2691bf46a92d078d674cbaa
JLin056 <jlin890516@gmail.com>
2025-10-16 15:09:18 +0800

    resolve bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { HotelListPage } from './pages/hotel-list-page/hotel-list-page';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePage
-    }
-
+    },
     {
         path: 'dogHotels',
         component: HotelListPage
