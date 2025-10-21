9e0510bf59723b6b631bd66840bcdba61d8e1678
Wayne-Huang123 <a0952274142@gmail.com>
2025-10-21 17:09:36 +0800

    order table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { HotelListPage } from './pages/hotel-list-page/hotel-list-page';
 import { UploadImg } from './shared/sharedComponents/upload-img/upload-img';
+import { OrderTableComponent } from './pages/order-table-component/order-table-component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'imageTest',
         component: UploadImg
+    },
+    {
+        path:'orderTable',
+        component:OrderTableComponent
     }
 ];
