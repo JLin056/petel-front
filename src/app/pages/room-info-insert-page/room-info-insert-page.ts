f5bb6abb9487598dd6ea22e9c1cffb88affc4e8a
00550399 <linnnnng910410@gmail.com>
2025-10-21 11:30:31 +0900

    修改[MerchView] 新增 房型資訊新增 切版頁面 --將房型介紹的輸入框修改成文字編輯器的樣式

--- src/app/pages/room-info-insert-page/room-info-insert-page.ts
+++ src/app/pages/room-info-insert-page/room-info-insert-page.ts
@@ -1,24 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
 
 @Component({
   selector: 'app-room-info-insert-page',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, EditorModule],
   templateUrl: './room-info-insert-page.html',
-  styleUrl: './room-info-insert-page.css'
+  styleUrl: './room-info-insert-page.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class RoomInfoInsertPage implements OnInit {
   roomForm!: FormGroup;
   constructor(private fb: FormBuilder) { }
+
   ngOnInit(): void {
     this.roomForm = this.fb.group({
       name: [''],
       description: [''],
       equipment: [''],
-      price: ['']
+      price: [''],
+      unit: ['']
     });
   }
+
   onSubmit(): void {
     console.log(this.roomForm.value);
     // 🔜 之後這裡改成呼叫 API，例如:
