5e77615bffe4b45a1bc4cd71b0f47dd5f5b7e6f5
Gationhe <kayonwang@gmail.com>
2025-10-26 17:47:43 +0800

    fix bug

--- src/app/pages/booking-done-page/booking-done-page.ts
+++ src/app/pages/booking-done-page/booking-done-page.ts
@@ -1,3 +1,5 @@
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 import { BookService, OrderData } from './../../core/services/book-service';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
@@ -28,7 +30,7 @@ export class BookingDonePage {
     /**
      * 建構子注入
      */
-    constructor(private bookService: BookService, private hotelService: HotelService) { };
+    constructor(private router: Router, private bookService: BookService, private hotelService: HotelService, private messageService: MessageService) { };
 
     /**
      * 初始化頁面內容
@@ -37,24 +39,40 @@ export class BookingDonePage {
 
         this.orderData = this.bookService.getSharedOrderData();
 
+        if (!this.orderData) {
+            this.messageService.add({ severity: 'warn', summary: 'Warn', detail: '資料傳輸異常，將導回 PETEL 首頁' });
+            this.router.navigateByUrl('/');
+        }
+
         this.hotelService.queryHotelDetail(this.orderData.propertyId).subscribe({
             next: (response) => {
                 if (response.MWHEADER.RETURNCODE !== '0000') {
+                    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: '旅館資訊載入異常' });
                     return;
                 }
                 const propertyDetail = response.TRANRS.property_details.at(0);
                 this.propertyName = propertyDetail!.name;
                 this.propertyTel = propertyDetail!.tel;
                 this.propertyAddress = propertyDetail!.address;
+            },
+            error: (error) => {
+                this.messageService.add({ severity: 'warn', summary: 'Warn', detail: '旅館資訊載入異常' });
+                return;
             }
         });
     }
 
+    /**
+     * 點擊聊聊按鈕，轉導至聊天室頁面
+     */
     onChat(): void {
-        console.log('開始聊天');
+        this.router.navigateByUrl('/chat'); // TODO Check: see whether it needs additional info.
     }
 
+    /**
+     * 點擊查看歷史訂單按鈕，轉導至會員資訊頁面
+     */
     onViewHistory(): void {
-        console.log('查看歷史訂單');
+        this.router.navigateByUrl('/history');
     }
 }
