49a423ef3b25c948a8e6ca3bde695d25b1185956
Gationhe <kayonwang@gmail.com>
2025-10-31 15:44:35 +0800

    temp store

--- src/app/core/interfaces/MERCH007Req.interface.ts
+++ src/app/core/interfaces/MERCH007Req.interface.ts
@@ -1,10 +1,39 @@
+// export interface MERCH007Tranrq {
+//     name: string;
+//     tel: string;
+//     address: string;
+//     bankAccount: string;
+//     info: string;
+//     checkNotice: string;
+//     petNotice: string;
+//     propertyNotice: string;
+// }
+
+// export interface Welcome {
+//     MWHEADER: Mwheader;
+//     TRANRQ:   Tranrq;
+// }
+
+// export interface Mwheader {
+//     MSGID: string;
+// }
+
 export interface MERCH007Tranrq {
-    name: string;
-    tel: string;
-    address: string;
-    bankAccount: string;
-    info: string;
-    checkNotice: string;
-    petNotice: string;
-    propertyNotice: string;
+    id:             string | undefined;
+    tel:            string | undefined;
+    city:           string | undefined;
+    district:       string | undefined;
+    addressDetail:  string | undefined;
+    bankAccount:    string | undefined;
+    info:           string | undefined;
+    checkNotice:    string | undefined;
+    petNotice:      string | undefined;
+    propertyNotice: string | undefined;
+    facilities:     string[] | undefined;
+    propertyImages: MERCH007TranrqPropertyImage[];
+}
+
+export interface MERCH007TranrqPropertyImage {
+    mediaId:        string;
+    sortOrder:      number | undefined;
 }
--- src/app/pages/merchant-property-edit-page/merchant-property-edit-page.ts
+++ src/app/pages/merchant-property-edit-page/merchant-property-edit-page.ts
@@ -1,15 +1,13 @@
-import { SingleHotelDetail } from './../../core/interfaces/HOTEL005Res.interface';
-import { HotelService } from './../../core/services/hotel-service';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { MERCH007Tranrq } from '../../core/interfaces/MERCH007Req.interface';
 import { MerchService } from '../../core/services/merch-service';
-import { PropertyStateService } from '../../core/services/property-state.service';
 import { SharedConfirmDialog } from '../shared-confirm-dialog/shared-confirm-dialog';
+import { HotelService } from './../../core/services/hotel-service';
 
 @Component({
     selector: 'app-merchant-property-edit-page',
@@ -24,8 +22,9 @@ import { SharedConfirmDialog } from '../shared-confirm-dialog/shared-confirm-dia
     templateUrl: './merchant-property-edit-page.html',
     styleUrl: './merchant-property-edit-page.css'
 })
-export class MerchantPropertyEditPage implements OnInit {
-    propertyForm!: FormGroup;
+export class MerchantPropertyEditPage {
+
+    propertyForm: FormGroup;
     cancelConfirmVisible = false;
     isSubmitting = false;
     errorMessage = '';
@@ -38,71 +37,53 @@ export class MerchantPropertyEditPage implements OnInit {
         private router: Router,
         private hotelService: HotelService
     ) {
-        const navigation = this.router.getCurrentNavigation();
-        if (navigation?.extras?.state) {
-            this.propertyData = navigation.extras.state['property'];
-            this.propertyId = this.propertyData?.id || '';
-            // console.log('接收到的旅館資料:', this.propertyData);
-            this.hotelService.querySingleHotelDetailForMerchant(this.propertyId).subscribe({
-                next: (response) => {
-                    if (response.MWHEADER.RETURNCODE === '0000') {
-                        const singleHotelDetail = response.TRANRS.singleHotelDetail;
-                        this.propertyData = singleHotelDetail;
-                        this.propertyForm.patchValue({
-                            city: this.propertyData.city,
-                            district: this.propertyData.district
-                        })
-                    }
-                },
-                error: (error) => {
-                    console.error('獲取會員資訊失敗:', error);
-                }
-            })
-        }
-    }
-
-    ngOnInit(): void {
-        this.initForm();
-        if (!this.propertyData || !this.propertyId) {
-            setTimeout(() => this.router.navigate(['/merchants/property/homepage']), 2000);
-            return;
-        }
-        this.populateForm();
-    }
-
-    private initForm(): void {
         this.propertyForm = this.fb.group({
             name: ['', Validators.required],
             businessCode: [{ value: '', disabled: true }],
             bankAccount: ['', Validators.required],
             tel: ['', Validators.required],
-            // address: ['', Validators.required],
-            city: ['', Validators.required], // new
-            district: ['', Validators.required], // new
-            address: ['', Validators.required], // new
+            city: ['', Validators.required],
+            district: ['', Validators.required],
+            address: ['', Validators.required],
             info: ['', Validators.required],
             checkNotice: ['', Validators.required],
             petNotice: ['', Validators.required],
-            propertyNotice: ['']
+            propertyNotice: [''],
+            facilities: [[]]
         });
-    }
 
-    private populateForm(): void {
-        if (!this.propertyData) return;
-        this.propertyForm.patchValue({
-            name: this.propertyData.name || '',
-            businessCode: this.propertyData.businessCode || '',
-            bankAccount: this.propertyData.bankAccount || '',
-            tel: this.propertyData.tel || '',
-            // address: this.propertyData.address || '',
-            city: this.propertyData.city || '',
-            district: this.propertyData.district || '',
-            address: this.propertyData.address || '',
-            info: this.propertyData.info || '',
-            checkNotice: this.propertyData.checkNotice || '',
-            petNotice: this.propertyData.petNotice || '',
-            propertyNotice: this.propertyData.propertyNotice || '',
-        });
+        const navigation = this.router.getCurrentNavigation();
+
+        if (navigation?.extras?.state) {
+            this.propertyId = navigation.extras.state['property']?.id || '';
+            this.hotelService.querySingleHotelDetailForMerchant(this.propertyId).subscribe({
+                next: (response) => {
+                    if (response.MWHEADER.RETURNCODE === '0000') {
+                        this.propertyData = response.TRANRS.singleHotelDetail;
+                        this.propertyForm.patchValue({
+                            name: this.propertyData.name || '',
+                            businessCode: this.propertyData.businessCode || '',
+                            bankAccount: this.propertyData.bankAccount || '',
+                            tel: this.propertyData.tel || '',
+                            city: this.propertyData.city || '',
+                            district: this.propertyData.district || '',
+                            address: this.propertyData.address || '',
+                            facilities: this.propertyData.facilities || '',
+                            info: this.propertyData.info || '',
+                            checkNotice: this.propertyData.checkNotice || '',
+                            petNotice: this.propertyData.petNotice || '',
+                            propertyNotice: this.propertyData.propertyNotice || '',
+                        });
+                    } else {
+                        setTimeout(() => this.router.navigate(['/merchants/property/homepage']), 2000);
+                    }
+                },
+                error: (error) => {
+                    console.error('獲取資訊失敗:', error);
+                    setTimeout(() => this.router.navigate(['/merchants/property/homepage']), 2000);
+                }
+            })
+        }
     }
 
     onSubmit(): void {
@@ -112,11 +93,25 @@ export class MerchantPropertyEditPage implements OnInit {
             return;
         }
 
-        const formData = this.propertyForm.value;
-        const tranrq = {
+        // const formData = this.propertyForm.value;
+        // const tranrq = {
+        //     id: this.propertyId,
+        //     sellerId: this.propertyData.sellerId,
+        //     ...formData
+        // };
+        const tranrq: MERCH007Tranrq = {
             id: this.propertyId,
-            sellerId: this.propertyData.sellerId,
-            ...formData
+            tel: this.propertyForm.controls['tel'].value,
+            city: this.propertyForm.controls['city'].value,
+            district: this.propertyForm.controls['district'].value,
+            addressDetail: this.propertyForm.controls['district'].value,
+            bankAccount: this.propertyForm.controls['bankAccount'].value,
+            info: this.propertyForm.controls['info'].value,
+            checkNotice: this.propertyForm.controls['checkNotice'].value,
+            petNotice: this.propertyForm.controls['petNotice'].value,
+            propertyNotice: this.propertyForm.controls['propertyNotice'].value,
+            facilities: this.propertyForm.controls['facilities'].value,
+            propertyImages: []
         };
 
         this.isSubmitting = true;
