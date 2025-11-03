f4858e54b431176b4b1f13690ed08e2dc65ec1d5
00550399 <linnnnng910410@gmail.com>
2025-11-03 17:51:52 +0900

    修改新增旅館的路徑

--- src/app/pages/merchant-property-insert-page/merchant-property-insert-page.ts
+++ src/app/pages/merchant-property-insert-page/merchant-property-insert-page.ts
@@ -349,7 +349,7 @@ export class MerchantPropertyInsertPage {
         next: (res) => {
           if (res.MWHEADER.RETURNCODE === '0000') {
             this.messageService.add({ severity: 'success', summary: '成功', detail: '旅館新增成功！' });
-            this.router.navigate(['/merchants/userPage']);
+            this.router.navigate(['/merchants/property/info']);
           } else {
             this.messageService.add({ severity: 'error', summary: '錯誤', detail: res.MWHEADER.RETURNDESC || '新增失敗' });
           }
