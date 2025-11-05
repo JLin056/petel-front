83e98b01702e3f732f99e9646a61c58ee28983a0
Arial-Huang <s9752085520@gmail.com>
2025-11-05 15:19:54 +0800

    修改前台首頁圖及footer

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -41,10 +41,10 @@ export class HomePage implements OnInit {
 
     // Banner 圖片列表
     bannerImages: BannerImage[] = [
-        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/you-deng-zai-sha-fa-shang-de-ke-ai-xiao-rong-gou.jpg', alt: 'Petel 廣告圖 1' },
-        { src: 'img/homePageLogo.png', alt: 'Petel 廣告圖 2' },
-        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/zai-shi-nei-fang-song-de-ke-ai-mao.jpg', alt: 'Petel 廣告圖 3' },
-        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/2206.i203.011.S.m004.c13.pet+services+cartoon.jpg', alt: 'Petel 廣告圖 4' },
+        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/PETEL+Demo+(2).jpg', alt: 'Petel 廣告圖 1' },
+        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/you-deng-zai-sha-fa-shang-de-ke-ai-xiao-rong-gou.jpg', alt: 'Petel 廣告圖 2' },
+        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/PETEL+Demo+(4).jpg', alt: 'Petel 廣告圖 3' },
+        { src: 'https://petelcathay-user.s3.us-east-1.amazonaws.com/Petel_footage/zai-shi-nei-fang-song-de-ke-ai-mao.jpg', alt: 'Petel 廣告圖 4' }
     ];
 
     // 搜尋參數
